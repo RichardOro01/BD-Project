28016d235f65f28e2e3aa8ae7cc90dbaae7b7a71
Richard.Oropesa <93955101+RichardOro01@users.noreply.github.com>
2023-05-09 02:05:47 -0400

    feat: libs

--- TRANSBUS/src/cu/edu/cujae/structbd/services/ServicesLocator.java
+++ TRANSBUS/src/cu/edu/cujae/structbd/services/ServicesLocator.java
@@ -1,4 +1,17 @@
 package cu.edu.cujae.structbd.services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ServicesLocator {
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("localhost", "admin", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
