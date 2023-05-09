5f6c0a75c8c5126f9968575ef83cb90639a3d10f
Richard.Oropesa <93955101+RichardOro01@users.noreply.github.com>
2023-05-09 01:00:16 -0400

    base app

---
+++ TRANSBUS/src/gui/App.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            App frame = new App();
+            frame.setVisible(true);
+        });
+    }
+    public App() {
+        setTitle("Transbus");
+        setBounds(0, 0, Definitions.APP_WIDTH, Definitions.APP_HEIGHT);
+        getContentPane().setLayout(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(getOwner());
+    }
+
+}
---
+++ TRANSBUS/src/gui/Definitions.java
@@ -0,0 +1,6 @@
+package gui;
+
+public class Definitions {
+    public static final int APP_WIDTH = 1080;
+    public static final int APP_HEIGHT = 640;
+}
